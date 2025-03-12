dacf2333071c94ed08c35a06e5616d49c8dc82d1
guillermoRF2001 <123741083+guillermoRF2001@users.noreply.github.com>
2025-03-12 18:15:01 +0100

    Update Filtros.svelte

--- src/lib/Filtros.svelte
+++ src/lib/Filtros.svelte
@@ -21,31 +21,3 @@
     </label>
   </div>
 </div>
-
-<style>
-  .filters {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 20px;
-    padding: 10px;
-  }
-
-  .filter-group {
-    display: flex;
-    flex-direction: column;
-    background: #317e46;
-    padding: 15px;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-    width: 200px;
-    height: 200px;
-  }
-
-  label {
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    font-weight:bolder;
-  }
-</style>
